c188d95202c145c805cd3e8e784d38b1d5491a5d
micko <micko@microserver.(none)>
2016-10-25 21:03:06 +0100

    updated delimiters

--- history_scripts/epoch_history.pl
+++ history_scripts/epoch_history.pl
@@ -3,25 +3,43 @@
 use warnings;
 use strict;
 
-my $fileIn = $ARGV[0];
-chomp($fileIn);
-my @details =  split('\.', $fileIn);
-#print @details;
-#print $details[6], "\n";
+my ($fileIn, @details, $count);
 
-open(my $fh, $fileIn) or die "Could not open file '$fileIn' $!";
-my $count = 0;
+begining();
 
-my @arry = <$fh>;
-foreach my $a (@arry) {
-  if ($a =~ /^#/) {
-    my $b = $arry[$count + 1];
-    chomp($a);
-    $a =~ s/#//;
-my $realtime = localtime($a);
-    chomp($b);
-    my $data = "${realtime}::${details[4]}::${details[-3]}::${details[-2]}::$b";
-    print $data, "\n";
+sub begining{
+  if (defined $ARGV[0]){
+    foreach (@ARGV){
+      $fileIn = $_;
+      chomp($fileIn);
+      @details =  split('\.', $fileIn);
+      print_history($fileIn);
+    }
+  }   else {
+    help_me();
   }
-  $count ++;
+}
+
+sub print_history{
+  open(my $fh, $_[0]) or die "Could not open file '$_[0]' $!";
+  $count = 0;
+  my @arry = <$fh>;
+  foreach my $a (@arry) {
+    if ($a =~ /^#/) {
+      my $b = $arry[$count + 1];
+      chomp($a);
+      $a =~ s/#//;
+      my $realtime = localtime($a);
+      chomp($b);
+      my $data = "${realtime}¦${details[4]}¦${details[-3]}¦${details[-2]}¦$b";
+      print $data, "\n";
+    }
+    $count ++;
+  }
+
+}
+
+sub help_me{
+   print "Usage:  get_history.pl <History File> \n";
+   exit 1;
 }
